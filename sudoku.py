6964d55b8da0c41ae27fb11ed0f14a5bfb6348e2
serenaseymour <116756446+serenaseymour@users.noreply.github.com>
2022-11-25 15:35:56 -0500

    Update sudoku.py
    Added more functionality to the graphics.

--- sudoku.py
+++ sudoku.py
@@ -4,97 +4,97 @@ from board import Board
 import pygame
 from pygame.locals import *
 
-
-
-
 #source: https://www.geeksforgeeks.org/introduction-to-pygame/
-class Square(pygame.sprite.Sprite):
-    def __init__(self):
-        super(Square, self).__init__()
+#source: https://www.geeksforgeeks.org/how-to-use-multiple-screens-on-pygame/
+#source: https://pythonprogramming.altervista.org/buttons-in-pygame/
 
-        # Define the dimension of the surface
-        self.surf = pygame.Surface((80, 35))
 
-        # Define the color of the surface
-        self.surf.fill((255, 128, 0))
-        self.rect = self.surf.get_rect()
 
-        """
-        square1 = Square()
-        square2 = Square()
-        square3 = Square()
 
-        # Define where the squares will appear on the screen
-        # Use blit to draw them on the screen surface
-        screen.blit(square1.surf, (90, 400))
-        screen.blit(square2.surf, (210, 400))
-        screen.blit(square3.surf, (330, 400))
-        """
-
+#colors
 grey = (224, 224, 224)
 white = (255, 255, 255)
 black = (0, 0, 0)
 orange = (255, 128, 0)
 lightblue = (153, 204, 255)
 
-# Define the dimensions of screen object
+pygame.init()
 screen = pygame.display.set_mode((500, 600))
+font = pygame.font.SysFont("arial", 40)
+clock = pygame.time.Clock()
 
 
 def home_screen():
-    font = pygame.font.SysFont('arial', 55, True, False)
-    font2 = pygame.font.SysFont('arial', 45, False, False)
-    font3 = pygame.font.SysFont('calibri', 35, True, False)
+    font = pygame.font.SysFont('franklingothic', 60, False, False)
+    font2 = pygame.font.SysFont('franklingothic', 45, False, False)
 
     text = font.render('Welcome to Sudoku', True, black, grey)
     welcome = text.get_rect()
     welcome.center = (500 // 2, 100)
 
-    text2 = font2.render('Select Game Mode:', True, lightblue, grey)
+    text2 = font2.render('Select Game Mode:', True, black, grey)
     mode = text.get_rect()
     mode.center = (310, 300)
-
-    text3 = font3.render(' EASY ', True, white, orange)
-    easy = text3.get_rect()
-    easy.center = (100, 400)
-
-    text4 = font3.render(' MEDIUM ', True, white, orange)
-    medium = text4.get_rect()
-    medium.center = (250, 400)
-
-    text5 = font3.render(' HARD ', True, white, orange)
-    hard = text5.get_rect()
-    hard.center = (400, 400)
 
     screen.fill(grey)
     screen.blit(text, welcome)
     screen.blit(text2, mode)
-    screen.blit(text3, easy)
-    screen.blit(text4, medium)
-    screen.blit(text5, hard)
 
 
+class Button:
+    def __init__(self, text, pos, font, bg="black", feedback=""):
+        self.x, self.y = pos
+        self.font = pygame.font.SysFont("calibri", font)
+        if feedback == "":
+            self.feedback = "text"
+        else:
+            self.feedback = feedback
+        self.change_text(text, bg)
+
+    def change_text(self, text, bg="black"):
+        self.text = self.font.render(text, 1, pygame.Color(white))
+        self.size = self.text.get_size()
+        self.surface = pygame.Surface(self.size)
+        self.surface.fill(bg)
+        self.surface.blit(self.text, (0, 0))
+        self.rect = pygame.Rect(self.x, self.y, self.size[0], self.size[1])
+
+    def show(self):
+        screen.blit(button1.surface, (self.x, self.y))
+
+    def click(self, event):
+        x, y = pygame.mouse.get_pos()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed()[0]:
+                if self.rect.collidepoint(x, y):
+                    self.change_text(self.feedback, 'lightblue')
+
+
+
+
+
+button1 = Button("EASY", (100, 400), 30, orange, "START EASY MODE")
+button2 = Button("MEDIUM", (200, 400), 30, orange, "START MEDIUM MODE")
+button3 = Button("HARD", (300, 400), 30, orange, "START HARD MODE")
 
 
 def main():
 
-    gameOn = True
-
-    while gameOn:
-        pygame.init()
-
-        # set the pygame window name
+    while True:
+        #set the pygame window name
         pygame.display.set_caption('Group_13_Sudoku')
-
         home_screen()
 
-        #THIS IS REQUIRED TO DISPLAY PYGAME GRAPHICS
-        pygame.display.flip()
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                quit()
+                pygame.quit()
+            button1.click(event)
 
+        button1.show()
+        clock.tick(30)
+        pygame.display.update()
+
+        pygame.display.flip()
 
 
 
